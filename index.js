64008aa8ccc204c96f75f127045d0a1225cd45cb
dedpnd <pr.deadpanda@gmail.com>
2018-10-18 11:57:11 +0300

    !

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 var net = require('net');
 
 // parse "80" and "localhost:80" or even "42mEANINg-life.com:80"
-var addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/;
+// var addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/;
 
-var addr = {
-    from: addrRegex.exec(process.argv[2]),
-    to: addrRegex.exec(process.argv[3])
-};
+// var addr = {
+//     from: addrRegex.exec(process.argv[2]),
+//     to: addrRegex.exec(process.argv[3])
+// };
 
-if (!addr.from || !addr.to) {
-    console.log('Usage: <from> <to>');
-    return;
-}
+// if (!addr.from || !addr.to) {
+//     console.log('Usage: <from> <to>');
+//     return;
+// }
 
-net.createServer(function(from) {
-    var to = net.createConnection({
-        host: addr.to[2],
-        port: addr.to[3]
-    });
-    from.pipe(to);
-    to.pipe(from);
-}).listen(addr.from[3], addr.from[2]);
+// net.createServer(function(from) {
+//     var to = net.createConnection({
+//         host: addr.to[2],
+//         port: addr.to[3]
+//     });
+//     from.pipe(to);
+//     to.pipe(from);
+// }).listen(addr.from[3], addr.from[2]);
 
+console.log(process.env.PORT)
 console.log("proxy start...")
